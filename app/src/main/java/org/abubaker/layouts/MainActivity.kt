831d7263a01925b45750d004e4957ae444ac591a
abubaker <mohammad_abubaker@yahoo.com>
2021-11-11 12:18:52 +0500

    Base setup

---
+++ app/src/main/java/org/abubaker/layouts/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.abubaker.layouts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.layouts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
